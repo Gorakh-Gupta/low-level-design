d04a0f3b3b897127144465804dea6b4eef7ae82f
Gorakh Gupta <gorakh.gupta23@gmail.com>
2025-11-28 16:12:13 +0530

    tic-tac-toe code added

---
+++ lldrunner/src/main/java/com/gorakh/lldrunner/LldrunnerApplication.java
@@ -0,0 +1,10 @@
+package com.gorakh.lldrunner;
+import com.gorakh.lldrunner.tictactoegame.TictactoeSystemDemo;
+
+public class LldrunnerApplication {
+
+	public static void main(String[] args) {
+		TictactoeSystemDemo.run();
+	}
+
+}
---
+++ lldrunner/src/test/java/com/gorakh/lldrunner/LldrunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorakh.lldrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldrunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
